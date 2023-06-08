9b4be0ca0e17845da7503a2e572a47b4b1344de5
TylerRice27 <Tylerrayrice2@gmail.com>
2023-06-08 15:32:34 -0600

    Producer done but need to setup Docker Files to allow connection to my localhost

--- kafka-basics/src/main/java/io/tyler/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/tyler/demos/kafka/ProducerDemo.java
@@ -1,7 +1,56 @@
 package io.tyler.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+
 public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        log.info("Hello World");
+
+        Properties properties = new Properties();
+//        properties.setProperty("key","value");
+
+//        This connects to Local Host
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+
+//    Example of connecting to a third party
+//    properties.SetProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//    properties.SetProperty("security.protocol", "SASL_SSL");
+//    There is more to this line just an example for now
+//    properties.SetProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=")
+//    properties.SetProperty("sasl.mechanism", "PLAIN")
+
+
+//    set Producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+//    Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+//        Create a Proudcer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+//        Send Data
+        producer.send(producerRecord);
+
+//        flush and close the producer
+//        the producer to send all data and block until done --synchronous
+        producer.flush();
+
+//        You can call flush if you want to but if you call close it will always flush
+
+//        Producer close will also flush all of your data as well s
+        producer.close();
     }
 }
