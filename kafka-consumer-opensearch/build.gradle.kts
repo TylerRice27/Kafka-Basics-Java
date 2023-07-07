2a2e9215ae5a7fee3db6a0705918aaf1013855bf
TylerRice27 <Tylerrayrice2@gmail.com>
2023-07-06 23:50:11 -0600

    OpenSearch Connection made and Indexes Created

---
+++ kafka-consumer-opensearch/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.4.0")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.7")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+
+//    OpenSearch Dependendcy
+    implementation("org.opensearch.client:opensearch-rest-high-level-client:2.8.0")
+
+//    GSON Google
+    implementation("org.netbeans.external:com-google-gson:RELEASE113")
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
