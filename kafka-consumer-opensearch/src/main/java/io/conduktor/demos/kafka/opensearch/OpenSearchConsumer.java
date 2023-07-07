99fee477dff26d00de0ef52cdf214e24c7d64917
TylerRice27 <Tylerrayrice2@gmail.com>
2023-07-06 23:35:36 -0600

    OpenSearch Connection and dealing with Auth and without Auth

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,8 +1,58 @@
 package io.conduktor.demos.kafka.opensearch;
 
+import org.opensearch.client.RestHighLevelClient;
+
 public class OpenSearchConsumer {
 
+
+    public static RestHighLevelClient createOpenSearchClient(){
+
+        String connString = "http://localhost:9200";
+
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
+    }
+
     public static void main(String[] args) {
+
+        //First create an OpenSearch Client
+
+        Res
+
+
+        //Create Our Kafka Client
+
+        //Code Logic
+
+
+        //Create Our Kafka Client
+
+
 
     }
 }
