da0293e4fa7082e6979130e25b48604d328f7deb
TylerRice27 <Tylerrayrice2@gmail.com>
2023-07-06 23:49:54 -0600

    OpenSearch Connection made and Indexes Created

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,13 +1,31 @@
 package io.conduktor.demos.kafka.opensearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
 
 public class OpenSearchConsumer {
 
 
     public static RestHighLevelClient createOpenSearchClient(){
+//        Connection String for Localhost and Docker
+//        String connString = "http://localhost:9092";
 
-        String connString = "http://localhost:9200";
+//        Bonsai Connection String
+        String connString = "https://t4xhf7sebf:k3qc8p6q8m@kafka-course-4974295163.us-west-2.bonsaisearch.net:443";
 
         // we build a URI from the connection string
         RestHighLevelClient restHighLevelClient;
@@ -38,12 +56,29 @@ public class OpenSearchConsumer {
         return restHighLevelClient;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
 
         //First create an OpenSearch Client
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
 
-        Res
+        // We need to create the index of OpenSearch if it does not exist
 
+        try(openSearchClient){
+
+            boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+
+            if(!indexExists){
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("The Wikimedia index has been created");
+            }else{
+                log.info("The Wikimedia index already exists ");
+            }
+
+
+        }
 
         //Create Our Kafka Client
 
@@ -51,6 +86,8 @@ public class OpenSearchConsumer {
 
 
         //Create Our Kafka Client
+
+        //Close Things
 
 
 
