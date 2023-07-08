d7556991344a7c04b98d00640ea74d0f321443f9
TylerRice27 <Tylerrayrice2@gmail.com>
2023-07-07 22:49:51 -0600

    OpenSearch and trying to get Docker 8080 back up and running

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -6,16 +6,25 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
 
@@ -56,6 +65,23 @@ public class OpenSearchConsumer {
         return restHighLevelClient;
     }
 
+    private static KafkaConsumer<String,String> createKafkaConsumer(){
+
+        String groupId = "consumer-opensearch-demo";
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+//        Create a Consumer
+       return new KafkaConsumer<>(properties);
+    }
+
     public static void main(String[] args) throws IOException {
 
         Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
@@ -63,9 +89,13 @@ public class OpenSearchConsumer {
         //First create an OpenSearch Client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
+        //Create Our Kafka Client
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
+
         // We need to create the index of OpenSearch if it does not exist
 
-        try(openSearchClient){
+        try(openSearchClient; consumer){
 
             boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
 
@@ -79,8 +109,30 @@ public class OpenSearchConsumer {
 
 
         }
+        //Sub the consumer
+        consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
 
-        //Create Our Kafka Client
+        while (true){
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+
+            int recordCount = records.count();
+            log.info("Received" + recordCount + "record(s)");
+
+            for (ConsumerRecord<String, String> record : records){
+
+                //Send the record into OpenSearch
+                IndexRequest indexRequest = new IndexRequest("wikimedia")
+                        .source(record.value(), XContentType.JSON);
+
+                IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                log.info(response.getId(), "This is inserting 1 document into OpenSearch");
+            }
+        }
+
 
         //Code Logic
 
